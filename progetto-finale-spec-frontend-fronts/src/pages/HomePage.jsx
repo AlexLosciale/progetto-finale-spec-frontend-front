50394fd0e630eab0f23bff1348c1f5bbff7e820c
AlexLosciale <losciale17@gmail.com>
2025-06-11 11:37:49 +0200

    add: start 2

---
+++ progetto-finale-spec-frontend-fronts/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+    return (
+        <div className="text-center">
+        <h1>Benvenuto nella tua app!</h1>
+        <p>
+            Questa è la tua app di esempio. Puoi iniziare a costruire le tue pagine
+            e i tuoi componenti qui.
+        </p>
+        </div>
+    );
+}
